aa2f99a08dc09518c385150346002abab7eedad2
Gowthami <109407683+chinny555@users.noreply.github.com>
2023-09-19 00:19:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+app = Flask(__name__)
 
-app=Flask(__name__)
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
 
-@app.route("/")
-def home():
-    return 'Hello World'
+@app.route('/hello/<username>') 
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
 
-
-if __name__=='__main__':
-    app.run()
+if __name__ == '__main__':
+    app.run()  
