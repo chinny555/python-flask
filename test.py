f7dd8025530cc53cef52ba191b9dca4ab9e32382
Gowthami <109407683+chinny555@users.noreply.github.com>
2023-09-19 16:44:13 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-import unittest
-import app
 
-class TestHello(unittest.TestCase):
-
-    def setUp(self):
-        app.app.testing = True
-        self.app = app.app.test_client()
-
-  
-   
