898c5af4a82ec094d72f8590efa468173f79acc8
Gowthami <109407683+chinny555@users.noreply.github.com>
2023-09-19 16:43:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,14 +8,5 @@ class TestHello(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
-    def test_hello(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
-    def test_hello_hello(self):
-        rv = self.app.get('/hello/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
+  
    
